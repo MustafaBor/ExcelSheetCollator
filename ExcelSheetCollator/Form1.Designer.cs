80159716c710287c9fe56f9ec5605e8cb3f0d988
MustafaBor <mustafa@roadlaunch.com>
2020-03-24 21:47:43 -0400

    Importing project

---
+++ ExcelSheetCollator/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Excel_Sheet_Collator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxFolder = new System.Windows.Forms.GroupBox();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.groupBoxUpdated = new System.Windows.Forms.GroupBox();
+            this.listBoxNewFiles = new System.Windows.Forms.ListBox();
+            this.buttonSelectFolder = new System.Windows.Forms.Button();
+            this.groupBoxFolder.SuspendLayout();
+            this.groupBoxUpdated.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxFolder
+            // 
+            this.groupBoxFolder.Controls.Add(this.listBoxFiles);
+            this.groupBoxFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxFolder.Location = new System.Drawing.Point(20, 7);
+            this.groupBoxFolder.Name = "groupBoxFolder";
+            this.groupBoxFolder.Size = new System.Drawing.Size(399, 369);
+            this.groupBoxFolder.TabIndex = 0;
+            this.groupBoxFolder.TabStop = false;
+            this.groupBoxFolder.Text = "Select Project";
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.ItemHeight = 18;
+            this.listBoxFiles.Location = new System.Drawing.Point(10, 27);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(378, 328);
+            this.listBoxFiles.TabIndex = 0;
+            this.listBoxFiles.SelectedIndexChanged += new System.EventHandler(this.listBoxFiles_SelectedIndexChanged);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.buttonRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRefresh.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonRefresh.Location = new System.Drawing.Point(269, 383);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(84, 30);
+            this.buttonRefresh.TabIndex = 1;
+            this.buttonRefresh.Text = "Load";
+            this.buttonRefresh.UseVisualStyleBackColor = false;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxSearch.Location = new System.Drawing.Point(515, 382);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(144, 26);
+            this.textBoxSearch.TabIndex = 2;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.buttonSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSearch.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonSearch.Location = new System.Drawing.Point(683, 382);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(84, 30);
+            this.buttonSearch.TabIndex = 3;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = false;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // groupBoxUpdated
+            // 
+            this.groupBoxUpdated.Controls.Add(this.listBoxNewFiles);
+            this.groupBoxUpdated.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxUpdated.Location = new System.Drawing.Point(438, 7);
+            this.groupBoxUpdated.Name = "groupBoxUpdated";
+            this.groupBoxUpdated.Size = new System.Drawing.Size(399, 369);
+            this.groupBoxUpdated.TabIndex = 1;
+            this.groupBoxUpdated.TabStop = false;
+            this.groupBoxUpdated.Text = "Search Result";
+            // 
+            // listBoxNewFiles
+            // 
+            this.listBoxNewFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxNewFiles.FormattingEnabled = true;
+            this.listBoxNewFiles.ItemHeight = 18;
+            this.listBoxNewFiles.Location = new System.Drawing.Point(11, 27);
+            this.listBoxNewFiles.Name = "listBoxNewFiles";
+            this.listBoxNewFiles.Size = new System.Drawing.Size(378, 328);
+            this.listBoxNewFiles.TabIndex = 0;
+            this.listBoxNewFiles.SelectedIndexChanged += new System.EventHandler(this.listBoxNewFiles_SelectedIndexChanged);
+            // 
+            // buttonSelectFolder
+            // 
+            this.buttonSelectFolder.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.buttonSelectFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSelectFolder.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonSelectFolder.Location = new System.Drawing.Point(72, 382);
+            this.buttonSelectFolder.Name = "buttonSelectFolder";
+            this.buttonSelectFolder.Size = new System.Drawing.Size(131, 30);
+            this.buttonSelectFolder.TabIndex = 5;
+            this.buttonSelectFolder.Text = "Select Folder";
+            this.buttonSelectFolder.UseVisualStyleBackColor = false;
+            this.buttonSelectFolder.Click += new System.EventHandler(this.buttonSelectFolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(860, 425);
+            this.Controls.Add(this.buttonSelectFolder);
+            this.Controls.Add(this.groupBoxUpdated);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.groupBoxFolder);
+            this.Name = "Form1";
+            this.Text = "Excel Sheet Collator";
+            this.groupBoxFolder.ResumeLayout(false);
+            this.groupBoxUpdated.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxFolder;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.GroupBox groupBoxUpdated;
+        private System.Windows.Forms.ListBox listBoxNewFiles;
+        private System.Windows.Forms.Button buttonSelectFolder;
+    }
+}
+
---
+++ ExcelSheetCollator/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Excel_Sheet_Collator
+{
+    public partial class Form1 : Form
+    {
+
+        // Set default directory
+        string currentDirectory = "C:\\";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Open the selected worksheet within the relevant excel file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listBoxFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxFiles.SelectedItems.Count == 1)
+            {
+                // Instantiate Excel application
+                Excel.Application xl2 = new Excel.Application();
+                xl2.Visible = true; // Excel visible to user
+
+                // Separate sheet name and file name for processing
+                string fullName = listBoxFiles.GetItemText(listBoxFiles.SelectedItem);
+                string[] stringSeparators = new string[] { "  -  " };
+                string[] fileSheet = fullName.Split(stringSeparators, StringSplitOptions.None);
+                string sheetName = fileSheet[0];
+                string fileName = fileSheet[1];
+
+                // Open an existing workbook
+                string workbookPath = currentDirectory + fileName;
+                Excel.Workbook wb2 = xl2.Workbooks.Open(workbookPath,
+                    0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, "",
+                    true, false, 0, true, false, false);
+
+                // Get sheet in workbook
+                Excel.Sheets excelSheets = wb2.Worksheets;
+                Excel.Worksheet wsh = (Excel.Worksheet)excelSheets[sheetName];
+                wsh.Activate(); // Activate the worksheet
+
+                listBoxFiles.SelectedItems.Clear(); // Clear selected item
+            }
+        }
+
+        /// <summary>
+        /// Load/Refresh the Item List Asynchronously
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            listBoxFiles.Items.Clear(); // clear all items after first refresh
+
+            try
+            {
+                var progress = new Progress<string>((x) => listBoxFiles.Items.Add(x));
+                await Task.Run(() => DoWork(progress));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Background thread to prevent freezing IO with large number of files
+        /// </summary>
+        /// <param name="progress"></param>
+        private void DoWork(IProgress<string> progress)
+        {
+            // Obtain files in directory
+            DirectoryInfo dinfo = new DirectoryInfo(currentDirectory);
+            FileInfo[] Files = dinfo.GetFiles("*.xlsx");
+
+            // Instantiate Excel application
+            Excel.Application xl = new Excel.Application();
+
+            // Read all files
+            foreach (FileInfo file in Files)
+            {
+                //listBoxFiles.Items.Add(file.Name); // uncomment to see all files in another listBox (need to another listBox)
+                Excel.Workbook wb = xl.Workbooks.Open(currentDirectory + file.Name, 0, true);
+
+                // Read and assign all sheets and relevant files
+                foreach (Excel.Worksheet ws in wb.Worksheets)
+                {
+                    string wsName = ws.Name;
+                    string fileName = file.Name;
+                    progress.Report(wsName + "  -  " + fileName);  
+                }
+            }
+        }
+
+        /// <summary>
+        /// Search for specific file names or sheet names
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            List<string> newItems = new List<string>();
+            foreach (object item in listBoxFiles.Items)
+            {
+                // Check if search keyword matches with any items in the list
+                if (item.ToString().ToLower().Contains(textBoxSearch.Text.ToLower())) // Case-insensitive
+                {
+                    newItems.Add(item.ToString());
+                }
+            }
+
+            listBoxNewFiles.Items.Clear(); //Clear all items after first load
+
+            // Add all matching items in the new ListBox
+            foreach (string item in newItems)
+            {
+                listBoxNewFiles.Items.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// /// Open the selected worksheet within the relevant excel file in the refined list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listBoxNewFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxNewFiles.SelectedItems.Count == 1)
+            {
+                // Instantiate Excel application
+                Excel.Application xl2 = new Excel.Application();
+                xl2.Visible = true; // Excel visible to user
+
+                // Separate sheet name and file name for processing
+                string fullName = listBoxNewFiles.GetItemText(listBoxNewFiles.SelectedItem);
+                string[] stringSeparators = new string[] { "  -  " };
+                string[] fileSheet = fullName.Split(stringSeparators, StringSplitOptions.None);
+                string sheetName = fileSheet[0];
+                string fileName = fileSheet[1];
+
+                // Open an existing workbook
+                string workbookPath = currentDirectory + fileName;
+                Excel.Workbook wb2 = xl2.Workbooks.Open(workbookPath,
+                    0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, "",
+                    true, false, 0, true, false, false);
+
+                // Get sheet in workbook
+                Excel.Sheets excelSheets = wb2.Worksheets;
+                Excel.Worksheet wsh = (Excel.Worksheet)excelSheets[sheetName];
+                wsh.Activate(); // Activate the worksheet
+                
+                listBoxNewFiles.SelectedItems.Clear(); // Clear selected item
+            }
+        }
+
+        /// <summary>
+        /// Select the folder all excel files and sheets will be drawn from
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonSelectFolder_Click(object sender, EventArgs e)
+        {
+            // Instantiate folder browser dialog
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            // Update directory based on chosen folder
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                currentDirectory = fbd.SelectedPath + "\\";
+        }
+    }
+}
